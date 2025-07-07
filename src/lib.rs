bd4132260bd71f1e452a7a7a61ba00055ca8f827
cocode <github.com@worldware.com>
2025-07-07 00:08:42 -0700

    checkin

---
+++ src/lib.rs
@@ -0,0 +1,8 @@
+pub mod basic_types;
+pub mod basic_lexer;
+pub mod basic_parser;
+pub mod basic_operators;
+pub mod basic_dialect;
+pub mod basic_functions;
+pub mod basic_symbols;
+pub mod basic_interpreter;
---
+++ src/main.rs
@@ -0,0 +1,36 @@
+use std::env;
+use std::fs;
+use std::process;
+use basic_rs::basic_parser::Parser;
+use basic_rs::basic_lexer::Lexer;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 2 {
+        eprintln!("Usage: {} <program.bas>", args[0]);
+        process::exit(1);
+    }
+
+    let program_path = &args[1];
+    match fs::read_to_string(program_path) {
+        Ok(source) => {
+            let mut lexer = Lexer::new(&source);            let tokens = lexer.tokenize().expect("Lexing failed");
+            let mut parser = Parser::new(tokens);
+            match parser.parse() {
+                Ok(program) => {
+                    // TODO: Run the program
+                    println!("Program parsed successfully!");
+                    process::exit(0);
+                }
+                Err(e) => {
+                    eprintln!("Parse error: {:?}", e);
+                    process::exit(1);
+                }
+            }
+        }
+        Err(e) => {
+            eprintln!("Error reading file {}: {}", program_path, e);
+            process::exit(1);
+        }
+    }
+}
